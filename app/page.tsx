a43c08930be3d4b464704fa8f1c73d1d365a3504
Andre Leonardo <andre.leonardocb@gmail.com>
2024-06-10 20:57:12 -0300

    chore: add huskyand lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import { FunctionComponent } from "react";
 
-interface homeProps {
-  
-}
- 
-const home: FunctionComponent<homeProps> = () => {
-  return ( 
-    <div className="bg-primary mx-2 p-2">
+const home: FunctionComponent = () => {
+  return (
+    <div className="mx-2 bg-primary p-2">
       <h1>Hello</h1>
     </div>
-   );
-}
- 
+  );
+};
+
 export default home;
