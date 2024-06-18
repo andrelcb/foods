2be61ec6c7165d66287f55e2d16cbf04d70be2a0
Andre Leonardo <andre.leonardocb@gmail.com>
2024-06-17 21:53:57 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import { FunctionComponent } from "react";
 import Header from "./_components/header";
+import Search from "./_components/search";
 
-const home: FunctionComponent = () => {
-  return <Header />;
+const home = () => {
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default home;
